31bf4a7784e4b0755429cd71fa850757616f4249
sreekanth-kk <68840592+sreekanth-kk@users.noreply.github.com>
2021-05-23 18:03:08 +0530

    Update database.config.js

--- server/config/database.config.js
+++ server/config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // url: 'mongodb://localhost:27017/vaccine-data'
-    url: "mongodb+srv://admin:1234@cluster0.1rrji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    
 }
